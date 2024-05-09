3029171bd4182cf6c73ab20e0f6afebbc72b19be
thcnnn <epoyanarmen@gmail.com>
2024-05-09 17:52:02 +0300

    added keys files and differ connections

--- client.py
+++ client.py
@@ -1,31 +1,41 @@
 import socket
 import pickle
+import os
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+def load_keys(filename):
+    with open(filename, "r") as f:
+        keys = f.read().strip().split(',')
+    return tuple(map(int, keys))
 
 HOST = '127.0.0.1'
 PORT = 8080
+COMMUNICATION_PORT = 8081
+client_keys_file = 'client_keys.txt'
 
+p, g, a = load_keys(client_keys_file)
+A = pow(g, a, p)
 sock = socket.socket()
 sock.connect((HOST, PORT))
-
-p, g, a = 17, 5, 3
-A = pow(g, a, p)
 sock.send(pickle.dumps((p, g, A)))
+
 B = pickle.loads(sock.recv(2048))
 K = pow(B, a, p)
+cipher = AES.new(K.to_bytes(16, byteorder='big'), AES.MODE_ECB)
 print('Shared Key (K):', K)
 
-# Дальнейшее общение с использованием симметричного ключа K
-cipher = AES.new(K.to_bytes(16, byteorder='big'), AES.MODE_ECB)
+sock.close()
+sock = socket.socket()
+sock.connect((HOST, COMMUNICATION_PORT))
+
 while True:
     message = input('Enter message: ')
     encrypted_message = cipher.encrypt(pad(message.encode(), AES.block_size))
     sock.send(encrypted_message)
     response = sock.recv(1024)
-    decrypted_response = unpad(cipher.decrypt(response), AES.block_size).decode()
+    decrypted_response = unpad(cipher.decrypt(encrypted_message), AES.block_size).decode()
     if decrypted_response == '__closeconn':
-        print('server closed connection')
+        print('Server closed connection')
         sock.close()
         break
     print('Received response:', decrypted_response)
--- server.py
+++ server.py
@@ -2,26 +2,39 @@ import socket
 import pickle
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+def load_keys(filename):
+    with open(filename, "r") as f:
+        keys = f.read().strip().split(',')
+    return tuple(map(int, keys))
 
 HOST = '127.0.0.1'
 PORT = 8080
-b = 2
+COMMUNICATION_PORT = 8081
+server_keys_file = 'server_keys.txt'
+
+b = load_keys(server_keys_file)[0]
+
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
+
 msg = conn.recv(1024)
 msg = pickle.loads(msg)
-p = msg[0]
-g = msg[1]
-A = msg[2]
+p, g, A = msg
+
 B = pow(g, b, p)
 K = pow(A, b, p)
 conn.send(pickle.dumps(B))
+conn.close()
 print('Shared Key (K):', K)
 
-# Дальнейшее общение с использованием симметричного ключа K
+sock = socket.socket()
+sock.bind((HOST, COMMUNICATION_PORT))
+sock.listen(1)
+conn, addr = sock.accept()
 cipher = AES.new(K.to_bytes(16, byteorder='big'), AES.MODE_ECB)
+
 while True:
     message = conn.recv(1024)
     decrypted_message = unpad(cipher.decrypt(message), AES.block_size).decode()
