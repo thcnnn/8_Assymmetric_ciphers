2eb44768b322d5b59115103e873156e7a71897bf
thcnnn <epoyanarmen@gmail.com>
2024-05-09 15:52:40 +0300

    Added diffie-hellman protocol server-client app

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 import pickle
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -7,8 +9,26 @@ PORT = 8080
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
+p, g, a = 17, 5, 3
+A = pow(g, a, p)
 sock.send(pickle.dumps((p, g, A)))
+B = pickle.loads(sock.recv(2048))
+K = pow(B, a, p)
+print('Shared Key (K):', K)
 
-sock.close()
+# Дальнейшее общение с использованием симметричного ключа K
+cipher = AES.new(K.to_bytes(16, byteorder='big'), AES.MODE_ECB)
+while True:
+    message = input('Enter message: ')
+    encrypted_message = cipher.encrypt(pad(message.encode(), AES.block_size))
+    sock.send(encrypted_message)
+    response = sock.recv(1024)
+    decrypted_response = unpad(cipher.decrypt(response), AES.block_size).decode()
+    if decrypted_response == '__closeconn':
+        print('server closed connection')
+        sock.close()
+        break
+    print('Received response:', decrypted_response)
+    if message == '__quit':
+        sock.close()
+        break
--- server.py
+++ server.py
@@ -1,15 +1,35 @@
 import socket
 import pickle
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 HOST = '127.0.0.1'
 PORT = 8080
-
+b = 2
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
-
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+msg = pickle.loads(msg)
+p = msg[0]
+g = msg[1]
+A = msg[2]
+B = pow(g, b, p)
+K = pow(A, b, p)
+conn.send(pickle.dumps(B))
+print('Shared Key (K):', K)
 
-conn.close()
+# Дальнейшее общение с использованием симметричного ключа K
+cipher = AES.new(K.to_bytes(16, byteorder='big'), AES.MODE_ECB)
+while True:
+    message = conn.recv(1024)
+    decrypted_message = unpad(cipher.decrypt(message), AES.block_size).decode()
+    print('Received message:', decrypted_message)
+
+    response = input('Enter response: ')
+    encrypted_response = cipher.encrypt(pad(response.encode(), AES.block_size))
+    conn.send(encrypted_response)
+    if response == '__closeconn':
+        conn.close()
+        break
